16dc5d7f77ad8f0f7ecf17b3fbfb750a3c2d365f
vikram-narayan <vnarayan@quantopian.com>
2018-06-25 14:53:31 -0400

    MAINT: reexport for backwards compat

--- zipline/utils/input_validation.py
+++ zipline/utils/input_validation.py
@@ -21,6 +21,7 @@ from pytz import timezone
 from six import iteritems, string_types, PY3
 from toolz import valmap, complement, compose
 import toolz.curried.operator as op
+from trading_calendars.trading_calendar import coerce
 
 from zipline.utils.compat import wraps
 from zipline.utils.functional import getattrs
@@ -755,43 +756,6 @@ def expect_dimensions(__funcname=_qualified_name, **dimensions):
             return argvalue
         return _check
     return preprocess(**valmap(_expect_dimension, dimensions))
-
-
-def coerce(from_, to, **to_kwargs):
-    """
-    A preprocessing decorator that coerces inputs of a given type by passing
-    them to a callable.
-
-    Parameters
-    ----------
-    from : type or tuple or types
-        Inputs types on which to call ``to``.
-    to : function
-        Coercion function to call on inputs.
-    **to_kwargs
-        Additional keywords to forward to every call to ``to``.
-
-    Examples
-    --------
-    >>> @preprocess(x=coerce(float, int), y=coerce(float, int))
-    ... def floordiff(x, y):
-    ...     return x - y
-    ...
-    >>> floordiff(3.2, 2.5)
-    1
-
-    >>> @preprocess(x=coerce(str, int, base=2), y=coerce(str, int, base=2))
-    ... def add_binary_strings(x, y):
-    ...     return bin(x + y)[2:]
-    ...
-    >>> add_binary_strings('101', '001')
-    '110'
-    """
-    def preprocessor(func, argname, arg):
-        if isinstance(arg, from_):
-            return to(arg, **to_kwargs)
-        return arg
-    return preprocessor
 
 
 def coerce_types(**kwargs):
